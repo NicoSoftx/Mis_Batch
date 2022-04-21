5ab3e6782245b17ae8600968836221d0ffe14d22
NicoSoftx <NicoSoftx@gmail.com>
2022-04-20 22:59:14 -0500

    Modifique una segunda ves el codigo

--- OBS_MUSIC.bat
+++ OBS_MUSIC.bat
@@ -4,3 +4,4 @@ cd E:\VIDEO\OBS-studio-ME\bin\64bit\
 start "" "E:\VIDEO\OBS-studio-ME\bin\64bit\obs64.exe"
 start "" "E:\SONIDO\Element\Element.exe"
 echo Listo linea acgregada.
+echo otra ves se cambio
