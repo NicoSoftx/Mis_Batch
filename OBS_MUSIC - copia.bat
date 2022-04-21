7a33d94522995657c24b3c21e0cb7a1bc404181f
Nicolas carvajal <NicoSoftx@gmail.com>
2022-04-20 22:53:15 -0500

    hola

---
+++ OBS_MUSIC - copia.bat
@@ -0,0 +1,6 @@
+nircmd speak text "Lanzando aplicaciones de Streaming" 
+E:
+cd E:\VIDEO\OBS-studio-ME\bin\64bit\
+start "" "E:\VIDEO\OBS-studio-ME\bin\64bit\obs64.exe"
+start "" "E:\SONIDO\Element\Element.exe"
+echo Listo linea acgregada.
